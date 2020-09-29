b6cfadf56e1fd5b2fa18f6526bb4710d393ef943
Donald Atkinson <FuzzicalLogic@users.noreply.github.com>
2020-09-28 18:00:34 -0600

    Add Dependency Branch: World Configurator

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ if (rn(localStorage.getItem("version"), 2) !== rn(version, 2)) {
 }
 
 import * as ThreeD from "./modules/ui/3d.js";
+import { editWorld } from "./modules/ui/world-configurator.js";
 
 // append svg layers (in default order)
 export let svg = d3.select("#map");
