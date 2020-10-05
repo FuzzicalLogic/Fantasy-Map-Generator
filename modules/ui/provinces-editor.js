40ae453c5a1e856955182e6362c3c4d16e876bbe
Donald Atkinson <FuzzicalLogic@users.noreply.github.com>
2020-10-04 23:55:09 -0600

    FIX: Missing Imports

--- modules/ui/provinces-editor.js
+++ modules/ui/provinces-editor.js
@@ -1,4 +1,8 @@
-import { modules, pack, defs, provs } from "../../main.js";
+import {
+    modules, pack, defs,
+    provs, provinceBorders, stateBorders, debug,
+    viewbox, zoomTo
+} from "../../main.js";
 
 import * as Names from "../names-generator.js";
 import * as BurgsAndStates from "../burgs-and-states.js";
@@ -6,7 +10,7 @@ import { editStates } from "./states-editor.js";
 
 import { closeDialogs, restoreDefaultEvents, moveCircle, removeCircle, fitContent, applySorting, moveBurgToGroup, openPicker, fog, unfog, getFileName, downloadFile } from "./editors.js";
 import { tip, applyOption, showMainTip, clearMainTip } from "./general.js";
-import { findCell, getPackPolygon, isLand, getRandomColor, rn, isCtrlClick, si, openURL } from "../utils.js";
+import { findCell, getPackPolygon, isLand, getRandomColor, rn, isCtrlClick, si, openURL, findAll } from "../utils.js";
 import { editStyle } from "./style.js";
 import {
     turnButtonOff, toggleCultures, toggleStates, drawStates, drawBorders, toggleBorders, toggleProvinces, drawProvinces, layerIsOn
