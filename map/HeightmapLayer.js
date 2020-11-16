1a0f1f2a371f800ef3e9e542edb170eea62c90dc
Donald Atkinson <FuzzicalLogic@users.noreply.github.com>
2020-11-15 17:14:09 -0700

    FIX: Heightmap is based on grid, not pack

--- map/HeightmapLayer.js
+++ map/HeightmapLayer.js
@@ -5,12 +5,11 @@ import * as generator from "../modules/heightmap-generator.js";
 
 generator.addEventListener('update', drawHeightmap)
 export function drawHeightmap({ detail:{ cells, vertices}}) {
-    console.time("drawHeightmap");
+    console.time("Heightmap.Render");
     let { terrs } = view;
     terrs.selectAll("*").remove();
-//    const { cells, vertices } = pack,
-        let n = cells.length;
-    const used = new Uint8Array(cells.length);
+    const n = cells.length;
+    const used = [];
     const paths = new Array(101).fill("");
 
     const scheme = getColorScheme();
@@ -34,17 +33,17 @@ export function drawHeightmap({ detail:{ cells, vertices}}) {
         .sort(({ h: h1 }, { h: h2 }) => h1 - h2)
 
     for (const { i, h, c, v } of heights) {
-        if (used[i])
+        if (used.includes(cells[i]))
             continue; // already marked
         const onborder = c.some(n => cells[n].h < h);
         if (!onborder)
             continue;
-        const vertex = v.find(v => vertices[v].c.some(i => cells[i].h < h));
+        const vertex = v.find(v => vertices.c[v].some(i => cells[i] && cells[i].h < h));
 
         const chain = connectVertices(vertex, h);
         if (chain.length < 3)
             continue;
-        const points = simplifyLine(chain).map(v => vertices[v].p);
+        const points = simplifyLine(chain).map(v => vertices.p[v]);
         paths[h] += round(lineGen(points));
     }
 
@@ -64,13 +63,13 @@ export function drawHeightmap({ detail:{ cells, vertices}}) {
         for (let i = 0, current = start; i === 0 || current !== start && i < 20000; i++) {
             const prev = chain[chain.length - 1]; // previous vertex in chain
             chain.push(current); // add current vertex to sequence
-            const c = vertices[current].c; // cells adjacent to vertex
-            c.filter(c => ~~(cells[c].h) === h)
-                .forEach(c => used[c] = 1);
+            const c = vertices.c[current]; // cells adjacent to vertex
+            c.filter(c => cells[c] && cells[c].h === h)
+                .forEach(c => used.push(cells[c]));
             const c0 = c[0] >= n || cells[c[0]].h < h;
             const c1 = c[1] >= n || cells[c[1]].h < h;
             const c2 = c[2] >= n || cells[c[2]].h < h;
-            const v = vertices[current].v; // neighboring vertices
+            const v = vertices.v[current]; // neighboring vertices
             if (v[0] !== prev && c0 !== c1)
                 current = v[0];
             else if (v[1] !== prev && c1 !== c2)
@@ -84,12 +83,11 @@ export function drawHeightmap({ detail:{ cells, vertices}}) {
         }
         return chain;
     }
+}
 
-    function simplifyLine(chain) {
-        if (!simplification) return chain;
-        const n = simplification + 1; // filter each nth element
-        return chain.filter((d, i) => i % n === 0);
-    }
-
-    console.timeEnd("drawHeightmap");
+function simplifyLine(chain) {
+    const simplification = +view.terrs.attr("relax");
+    if (!simplification) return chain;
+    const n = simplification + 1; // filter each nth element
+    return chain.filter((d, i) => i % n === 0);
 }
