5de7cf9b11bf8bae79f69fa5b080b13c580232a3
Donald Atkinson <FuzzicalLogic@users.noreply.github.com>
2020-11-25 03:35:16 -0700

    Add 'show' event

--- gui/TabView.js
+++ gui/TabView.js
@@ -2,7 +2,8 @@ export const TabView = el => {
     let buttons = () => el.querySelector('.tab');
     let contents = () => el.querySelectorAll('.tabcontent');
 
-    buttons().addEventListener('click', ({ target }) => {
+    buttons().addEventListener('click', e => {
+        let target = e.target;
         let prev = el.querySelector('.tab > .active');
         if (prev === target) return;
 
@@ -13,8 +14,13 @@ export const TabView = el => {
         let idx = Array.from(buttons().children).indexOf(target);
         contents().forEach(hide);
         show(contents()[idx]);
+
+        e.stopPropagation();
     })
 }
 
 const hide = el => el.setAttribute('hidden', '');
-const show = el => el.removeAttribute('hidden');
+const show = el => {
+    el.removeAttribute('hidden');
+    el.dispatchEvent(new CustomEvent('show'));
+}
